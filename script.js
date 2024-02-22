cf5a2cae7ab6eb2e1ac54d699fd6e0a7ffdce458
Henrique de Padua Valle <115121902+HenriquePValle@users.noreply.github.com>
2024-02-22 08:55:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ nome = prompt("Digite o seu nome:");
 console.log(nome);
 window.alert(`Olá ${nome}!`);
 let numero = Number.parseInt(Math.random()*10 + 1);
-let tentativas = 0;
+let numeroTentativas = 0;
+let tentativas = [];
 let palpite = 0;
 
+
 //&& -> operação lógica AND (E)
-while((palpite != numero) && (tentativas < 5)){
+while((palpite != numero) && (numeroTentativas < 5)){
     palpite = prompt("Digite um número: ");
-    tentativas++;
+    numeroTentativas++;
+    tentativas.push(palpite);
     if(palpite > numero){
         window.alert(`Você errou! O número é menor que ${palpite}. Tente outra vez.`);
     } else {
@@ -20,18 +23,12 @@ while((palpite != numero) && (tentativas < 5)){
 }
 
 if(numero == palpite) {
-    let palavraTentativa = tentativas==1 ? "tentativa" : "tentativas";
-    // if(tentativas == 1)
-    //     palavraTentativa = "tentativa";
-    // else
-    //     palavraTentativa = "tentativas";
-    window.alert(`Parabéns ${nome}! O número era ${numero}, e você acertou com ${tentativas} ${palavraTentativa}!!`);
+    let palavraTentativa = numeroTentativas==1 ? "tentativa" : "tentativas";
+    window.alert(`Parabéns ${nome}! O número era ${numero}, e você acertou com ${numeroTentativas} ${palavraTentativa}!!`);
 } else {
     window.alert(`Você esgotou suas tentativas e não acertou. O número era ${numero}!`);
 }
 
-// let numeroAleatorio = Math.random();
-// numeroAleatorio = (numeroAleatorio * 10) + 1;
-// numeroAleatorio = Number.parseInt(numeroAleatorio);
-
-// console.log(numeroAleatorio);
+for(let i = 0; i < tentativas.length; i++){
+    console.log(tentativas[i]);
+}
